ac9b25c7cf2f8f87825552e532c4dfbc666b818f
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-08-17 11:43:49 +0530

    Create java13.java

---
+++ java13.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		        Scanner in=new Scanner(System.in);
+        String inputString = in.nextLine();
+        Stack stack = new Stack();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            stack.push(inputString.charAt(i));
+        }
+
+        String reverseString = "";
+
+        while (!stack.isEmpty()) {
+            reverseString = reverseString+stack.pop();
+        }
+
+        if (inputString.equals(reverseString))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+
+	}
+}
