f26d80e6dad6f2e1cffeb2aa0a35c664189f21de
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-09-25 15:08:13 +0530

    Create java0808.java

---
+++ java0808.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class pro8
+{
+  public static void main(String[] args) throws java.lang.Exception
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int k=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=sc.nextInt();
+    }
+    for(int i=0;i<k;i++)
+    {
+      int b=sc.nextInt();
+      int c=sc.nextInt();
+      if(b==c||k==1)
+      {
+        System.out.println(a[b-1]);
+       }
+       else
+       {
+        while(b!=c)
+        {
+          if(b>c)
+          {
+              b=b-c;
+           }
+           else
+           {
+           c=c-b;
+           }
+           }
+           System.out.print(b);
+           }
+       }
+   }
+}
